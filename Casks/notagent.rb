2b42df9a563d36235d6d91e70a5d7bf8b3f6e39d
notagentdev <noreply@notagent.dev>
2026-09-25 13:47:37 +0200

    feature: update notagent to 0.1.63

--- Casks/notagent.rb
+++ Casks/notagent.rb
@@ -1,6 +1,6 @@
 cask "notagent" do
-  version "0.1.61"
-  sha256 "286d1b3ec41c3d19e3f5d693d138612f552b85aa71a3f06a23265947451ddc88"
+  version "0.1.63"
+  sha256 "4b908a7afcedb0f72378dc73bfff1481f8f69602dc82b6e62c8d827628326dd5"
 
   url "https://github.com/notagentdev/notagent/releases/download/v#{version}/notagent-v#{version}-aarch64-apple-darwin.tar.gz"
   name "notagent"
